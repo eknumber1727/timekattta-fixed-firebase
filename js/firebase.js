503dc9a9fe2d0cf8f7159446b18a64ddd1123287
eknumber1727 <eknumber1727@gmail.com>
2025-10-27 18:07:42 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYaFWLkkXn-fVswEb_FI1qEN_zfv3_6H8",
+  authDomain: "tk-app-01.firebaseapp.com",
+  projectId: "tk-app-01",
+  storageBucket: "tk-app-01.firebasestorage.app",
+  messagingSenderId: "338100477133",
+  appId: "1:338100477133:web:240c78504511ecee095d84",
+  measurementId: "G-YHZQBPL3NK"
+};
