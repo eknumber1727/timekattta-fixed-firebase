2373ed4c703c933096afdea612dd0b1d87df13ad
eknumber1727 <eknumber1727@gmail.com>
2025-10-27 18:06:16 +0530

    Update auth.js

--- js/modules/auth.js
+++ js/modules/auth.js
@@ -1,1 +1,26 @@
+import { initializeApp, getApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js';
+import { firebaseConfig } from '../firebase.js';
 
+let app, auth;
+
+export function ensureApp() {
+  try { app = getApp(); } catch { app = initializeApp(firebaseConfig); }
+  if (!auth) auth = getAuth();
+  return { app, auth };
+}
+
+export function watchAuth(callback) {
+  ensureApp();
+  return onAuthStateChanged(auth, callback);
+}
+
+export function emailPasswordLogin(email, password) {
+  ensureApp();
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  ensureApp();
+  return signOut(auth);
+}
